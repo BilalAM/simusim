f8f6c6d9e996f0c983de82783dbaa2879e930657
BilalAM <bilal.asif.97@gmail.com>
2020-05-02 21:53:06 +0500

    Added functionality to create summary files of simulation .

--- src/main/java/testing/TestDriverHomogeneous.java
+++ src/main/java/testing/TestDriverHomogeneous.java
@@ -1,15 +1,22 @@
 package testing;
 
+import freemarker.template.Configuration;
+import freemarker.template.Template;
 import logical.Tuple;
 import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
 import physical.FogNode;
 import physical.Sensor;
-import physical.Topology;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.StringWriter;
 import java.time.Duration;
 import java.time.Instant;
+import java.time.LocalDateTime;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 
 /**
@@ -41,7 +48,7 @@ import java.util.List;
  */
 public class TestDriverHomogeneous {
     private static Logger LOGGER = Logger.getLogger(TestDriverHomogeneous.class);
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
             FogNode f1 = createHomogeneousFogNode(1,"f1",0);
             FogNode f2 = createHomogeneousFogNode(2,"f2",0);
             FogNode f3 = createHomogeneousFogNode(3,"f3",1);
@@ -51,31 +58,81 @@ public class TestDriverHomogeneous {
 
             //establish connections
             s1.connectTo(f1);
-            s2.connectTo(f1);
+            s2.connectTo(f2);
             f1.connectTo(f3);
             f2.connectTo(f3);
 
-
-
-
             List<Tuple> testTuples = TestDriverUtils.createTuples(10);
-            List<Tuple> testTuples2 = TestDriverUtils.createTuples(50);
             Instant start = Instant.now();
-
-
 
                for (Tuple t : testTuples) {
                    new Thread(() -> s1.sendTuple(t, f1)).start();
-                   s2.sendTuple(t, f1);
+                   s2.sendTuple(t, f2);
                }
 
             Instant end = Instant.now();
         long seconds = Duration.between(start,end).getSeconds();
         LOGGER.log(Level.INFO,"Simulation took [ " + seconds + " ] to complete ");
-
+        appendResultsToFile(f1,f2,f3);
     }
 
 
+    /**
+     * Appends all the result summary of the fognodes into their corresonding FTL files which are then
+     * resolved into txt files created at the root folder of project .
+     * @param f1 : Fognode 1
+     * @param f2 : Fognode 2
+     * @param f3 : Fognode 3
+     * @throws Exception
+     */
+    private static void appendResultsToFile(FogNode f1 , FogNode f2 , FogNode f3) throws Exception {
+        Configuration cfg = new Configuration(Configuration.VERSION_2_3_23);
+
+        cfg.setClassForTemplateLoading(TestDriverHomogeneous.class, "/");
+        cfg.setDefaultEncoding("UTF-8");
+
+        Template template = cfg.getTemplate("ftls/f1-result.ftl");
+
+        Map<String, Object> templateData = new HashMap<>();
+        templateData.put("totalTuples", f1.getTuple().size());
+        templateData.put("dateTime", LocalDateTime.now().toString());
+        templateData.put("connectedDevices",f1.getConnectedFogNodes().toString());
+        templateData.put("rams",f1.utilizationHistory);
+        templateData.put("tuples",f1.tupleTimeHistory);
+
+        try (FileWriter out = new FileWriter(new File("f1-result-final.txt"),false)) {
+            template.process(templateData, out);
+            out.flush();
+        }
+
+        Template template2 = cfg.getTemplate("ftls/f2-result.ftl");
+
+        Map<String, Object> templateData2 = new HashMap<>();
+        templateData2.put("totalTuples", f2.getTuple().size());
+        templateData2.put("dateTime", LocalDateTime.now().toString());
+        templateData2.put("connectedDevices",f2.getConnectedFogNodes().toString());
+        templateData2.put("rams",f2.utilizationHistory);
+        templateData2.put("tuples",f2.tupleTimeHistory);
+
+        try (FileWriter out = new FileWriter(new File("f2-result-final.txt"),false)) {
+            template2.process(templateData2, out);
+            out.flush();
+        }
+
+        Template template3 = cfg.getTemplate("ftls/f3-result.ftl");
+
+        Map<String, Object> templateData3 = new HashMap<>();
+        templateData3.put("totalTuples", f3.getTuple().size());
+        templateData3.put("dateTime", LocalDateTime.now().toString());
+        templateData3.put("connectedDevices",f3.getConnectedFogNodes().toString());
+        templateData3.put("rams",f3.utilizationHistory);
+        templateData3.put("tuples",f3.tupleTimeHistory);
+
+        try (FileWriter out = new FileWriter(new File("f3-result-final.txt"),false)) {
+            template3.process(templateData3, out);
+            out.flush();
+        }
+    }
 
 
     public static FogNode createHomogeneousFogNode(int id, String name, int level) {
--- src/main/java/testing/TestDriverUtils.java
+++ src/main/java/testing/TestDriverUtils.java
@@ -1,16 +1,18 @@
 package testing;
 
 import logical.Tuple;
+import org.apache.commons.text.StringSubstitutor;
 import physical.Sensor;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 public class TestDriverUtils {
 
     public static List<Tuple> createTuples(int size) {
         List<Tuple> tuples = new ArrayList<>();
-        for (int i = 0; i <= size; i++) {
+        for (int i = 0; i < size; i++) {
             tuples.add(new Tuple(i, "source-A", "source-B"));
         }
         return tuples;
@@ -22,5 +24,9 @@ public class TestDriverUtils {
         return sensor;
     }
 
+    public static String replaceString(String toBeReplaced, Map<String, String> map) {
+        StringSubstitutor substitutor = new StringSubstitutor(map);
+        return substitutor.replace(toBeReplaced);
+    }
 
 }
